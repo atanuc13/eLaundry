0255e38749e42acd147d251411f8378d2d937202
Atanu Chatterjee <atanuc13@gmail.com>
2023-02-20 11:34:17 +0530

    path changed for admin service

--- adminService/src/admin.js
+++ adminService/src/admin.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const bodyparser= require("body-parser");
-const admin=require('./routes/Admin');
+const bodyparser = require("body-parser");
+const admin = require('./routes/Admin');
 require('dotenv/config');
 var cors = require('cors');
 var mongoose = require('mongoose');
-const app =express();
+const app = express();
 
 
 //Middlewar
@@ -12,18 +12,17 @@ app.use(cors());
 app.use(bodyparser.json());
 
 // Route
-app.use('/Admin',admin);
- 
+app.use('/admin', admin);
+
 // run server inn port no. 3000
-app.listen( process.env.PORT||3000);
+const port = process.env.PORT || 3000;
+app.listen(port);
 
 //database
 mongoose.set('strictQuery', false);
-mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser:true},function(err, db) {
-     if (err) throw err;
-     console.log("Connected");
-
-   
-   });
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, function(err, db) {
+    if (err) throw err;
+    console.log("Connected to port", port);
 
 
+});
--- adminService/src/routes/Admin.js
+++ adminService/src/routes/Admin.js
@@ -1,83 +1,78 @@
 const express = require("express");
 const upload = require("../middleware/upload");
-const router =express.Router();
+const router = express.Router();
 
-router.get ('/',(req,res)=>{
+router.get('/', (req, res) => {
     res.send('we are in admin');
     // we will call html here.
 });
 
 // For Institution
-router.get ('/Institutions',async (req,res)=>{
-    const institution=require('../models/institution');
-    try{
-    const inst = await institution.find();
-    res.json(inst);
-    }
-    catch(err){
+router.get('/institutions', async(req, res) => {
+    const institution = require('../models/institution');
+    try {
+        const inst = await institution.find();
+        res.json(inst);
+    } catch (err) {
         res.json(err);
     }
 });
 
 //Add new institutions 
-router.post ('/Institutions',async (req,res)=>{
-    const institution=require('../models/institution');
-   const inst=new institution({
-        name:req.body.name
+router.post('/institutions', async(req, res) => {
+    const institution = require('../models/institution');
+    const inst = new institution({
+        name: req.body.name
     });
-    try{
-   const saveInstitution= await inst.save();
-   res.json(saveInstitution);
-    }
-    
-    catch(err){
-        res.json({message:err});
+    try {
+        const saveInstitution = await inst.save();
+        res.json(saveInstitution);
+    } catch (err) {
+        res.json({ message: err });
     }
 
-    
+
 });
 
 // For Services
 
 //Show services
-router.get ('/Services',async (req,res)=>{
-    const services=require('../models/services');
-try{
-const serv = await services.find();
-res.json(serv);
-}
-catch(err){
-    res.json(err);
-}
-    
-   
+router.get('/services', async(req, res) => {
+    const services = require('../models/services');
+    try {
+        const serv = await services.find();
+        res.json(serv);
+    } catch (err) {
+        res.json(err);
+    }
+
+
 });
 
-router.post ('/Services',upload.single('imgPath'),async(req,res)=>{
-    const services=require('../models/services');
+router.post('/services', upload.single('imgPath'), async(req, res) => {
+    const services = require('../models/services');
     let img
-    if(req.file){
+    if (req.file) {
         img = req.file.path
-        
+
     }
     console.log(img)
     const serv = new services({
-        name:req.body.name,
-        imgPath:img
+        name: req.body.name,
+        imgPath: img
     });
-    try{
-    const saveService = await serv.save();
-    res.json(saveService);
+    try {
+        const saveService = await serv.save();
+        res.json(saveService);
+    } catch (err) {
+        res.json({ message: err });
     }
-    catch(err){
-res.json({message: err});
-    }
-   
+
 });
 
-router.put ('/Services',(req,res)=>{
+router.put('/services', (req, res) => {
     res.send('editServices');
-   
+
 });
 
-module.exports=router;
+module.exports = router;
