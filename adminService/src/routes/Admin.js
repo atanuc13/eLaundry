000ce13c141d168899ca87d9783173924041905b
Atanu Chatterjee <atanuc13@gmail.com>
2023-04-19 19:00:08 +0530

    ingress gateway path corrected

--- adminService/src/routes/Admin.js
+++ adminService/src/routes/Admin.js
@@ -1,101 +1,94 @@
 const express = require("express");
 const upload = require("../middleware/upload");
 const auth = require('../middleware/auth');
-const router =express.Router();
+const router = express.Router();
 
-router.get ('/', auth, (req,res)=>{
+router.get('/', auth, (req, res) => {
     res.send('we are in admin');
     // we will call html here.
 });
 
 // For Institution
-router.get ('/institutions', async (req,res)=>{
-    const institution=require('../models/institution');
-    try{
-    const inst = await institution.find();
-    res.json(inst);
-    }
-    catch(err){
+router.get('/institutions', async(req, res) => {
+    const institution = require('../models/institution');
+    try {
+        const inst = await institution.find();
+        res.json(inst);
+    } catch (err) {
         res.json(err);
     }
 });
 
 //Add new institutions 
-router.post ('/institutions', auth, async (req,res)=>{
-    const institution=require('../models/institution');
-   const inst=new institution({
-        name:req.body.name
+router.post('/institutions', auth, async(req, res) => {
+    const institution = require('../models/institution');
+    const inst = new institution({
+        name: req.body.name
     });
-    try{
-   const saveInstitution= await inst.save();
-   res.json(saveInstitution);
-    }
-    
-    catch(err){
-        res.json({message:err});
+    try {
+        const saveInstitution = await inst.save();
+        res.json(saveInstitution);
+    } catch (err) {
+        res.json({ message: err });
     }
 
-    
+
 });
 
 // Delete institution
-router.delete('/institutions', auth, async(req,res) => {
-    const inst=require('../models/institution');
+router.delete('/institutions', auth, async(req, res) => {
+    const inst = require('../models/institution');
     console.log(req.body.id);
-    try{
-    //console.log(inst.findById(req.body.id));
-    await inst.findByIdAndRemove(req.body.id);
-    res.status(200).send("Deleted");
-}
-catch(err){
-    res.json(err);
-}
+    try {
+        //console.log(inst.findById(req.body.id));
+        await inst.findByIdAndRemove(req.body.id);
+        res.status(200).send("Deleted");
+    } catch (err) {
+        res.json(err);
+    }
 });
 
 
 // For Services
 
 //Show services
-router.get ('/services', async (req,res)=>{
-    const services=require('../models/services');
-try{
-const serv = await services.find();
-res.json(serv);
-}
-catch(err){
-    res.json(err);
-}
-    
-   
+router.get('/services', async(req, res) => {
+    const services = require('../models/services');
+    try {
+        const serv = await services.find();
+        res.json(serv);
+    } catch (err) {
+        res.json(err);
+    }
+
+
 });
 
-router.post ('/services', auth, upload.single('imgPath'),async(req,res)=>{
-    const services=require('../models/services');
+router.post('/services', auth, upload.single('imgPath'), async(req, res) => {
+    const services = require('../models/services');
     let img
-    if(req.file){
+    if (req.file) {
         img = req.file.path
-    console.log(img)
-    const serv = new services({
-        name:req.body.name,
-        imgPath:img
-    });
-    try{
-    const saveService = await serv.save();
-    res.status(200).json(saveService);
-    }
-    catch(err){
-        res.json({message: err});
-            }
-        }
-        else{
-            res.status(400).send({"message":"Please select image"});
-        }
+        console.log(img)
+        const serv = new services({
+            name: req.body.name,
+            imgPath: img
         });
+        try {
+            const saveService = await serv.save();
+            res.status(200).json(saveService);
+        } catch (err) {
+            res.json({ message: err });
+        }
+    } else {
+        res.status(400).send({ "message": "Please select image" });
+    }
+});
 
-router.patch ('/services', auth, upload.single('imgPath'),async(req,res)=>{
-    const services=require('../models/services');
-    let img=null
-    
+router.patch('/services', auth, upload.single('imgPath'), async(req, res) => {
+    const services = require('../models/services');
+    let img = null
+
     //console.log("image: ",img)
     const serviceId = req.body.id
     console.log(serviceId)
@@ -103,58 +96,53 @@ router.patch ('/services', auth, upload.single('imgPath'),async(req,res)=>{
     const service = await services.findById(serviceId)
     console.log(service._id)
     console.log(service.imgPath)
-    if("file" in req){
-        img=req.file.path
-        fs.unlink("./"+service.imgPath, (err) => {
+    if ("file" in req) {
+        img = req.file.path
+        fs.unlink("./" + service.imgPath, (err) => {
             if (err) {
                 console.log("No previous file exist.");
                 res.status(400).send("No previous file exist.");
-            }
-            else
-            console.log("Delete File successfully.");
+            } else
+                console.log("Delete File successfully.");
         });
-            }
-            else 
-            {
-                img=service.imgPath
-            }
-    if(!(req.body.name)){
-        
-        console.log("name not preset")
-    req.name=service.name
+    } else {
+        img = service.imgPath
     }
-        await services.findOneAndUpdate({_id:service._id},{name:req.body.name,imgPath:img});
-        res.status(200).send("Details Updated successfully");
+    if (!(req.body.name)) {
+
+        console.log("name not preset")
+        req.name = service.name
+    }
+    await services.findOneAndUpdate({ _id: service._id }, { name: req.body.name, imgPath: img });
+    res.status(200).send("Details Updated successfully");
 
 });
 
 // Service delete
 
-router.delete('/services', auth, async(req,res) => {
-    const serv=require('../models/services');
+router.delete('/services', auth, async(req, res) => {
+    const serv = require('../models/services');
     console.log(req.body.id);
-    try{
+    try {
         const serviceId = req.body.id
-    console.log(serviceId)
-    const fs = require('fs');
-    const service = await serv.findById(serviceId)
-    console.log(service._id)
-    console.log(service.imgPath)
-    fs.unlink("./"+service.imgPath, (err) => {
-        if (err) {
-            console.log("No previous file exist.");
-            res.status(400).send("No previous file exist.");
-        }
-        else
-        console.log("Delete File successfully.");
-    })
+        console.log(serviceId)
+        const fs = require('fs');
+        const service = await serv.findById(serviceId)
+        console.log(service._id)
+        console.log(service.imgPath)
+        fs.unlink("./" + service.imgPath, (err) => {
+            if (err) {
+                console.log("No previous file exist.");
+                res.status(400).send("No previous file exist.");
+            } else
+                console.log("Delete File successfully.");
+        })
 
-    //console.log(inst.findById(req.body.id));
-    await serv.findByIdAndRemove(req.body.id);
-    res.status(200).send("Deleted");
-}
-catch(err){
-    res.json(err);
-}
+        //console.log(inst.findById(req.body.id));
+        await serv.findByIdAndRemove(req.body.id);
+        res.status(200).send("Deleted");
+    } catch (err) {
+        res.json(err);
+    }
 });
-module.exports=router;
+module.exports = router;
--- orderService/app.js
+++ orderService/app.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv/config');
-const eurekaHelper = require('./eureka-helper');
 //middlewares
 app.use(cors());
 app.use(bodyParser.json()) //convert the incoming request in json format(table entry S)
@@ -28,5 +27,3 @@ mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true },
 const port = process.env.port || 4004;
 //How to we start listening to the server
 app.listen(port);
-
-eurekaHelper.registerWithEureka('order-service', port);
